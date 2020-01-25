e9999679bf2e9d53899239468047b78af604c068
Tjebbe <tjebbemarchand@icloud.com>
2020-01-25 08:44:38 +0100

    Created navbar with books context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import BookContextProvider from "./contexts/BookContext";
+import Navbar from "./components/Navbar";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <BookContextProvider>
+                <Navbar />
+            </BookContextProvider>
+        </div>
+    );
 }
 
 export default App;
