2eddceb9375bccaed2fef0de619ff26a73911e8d
Tjebbe <tjebbemarchand@icloud.com>
2020-01-26 15:05:39 +0100

    Updates

---
+++ precache-manifest.cd0b6fb7be6104c073c901d57e6cee60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82730ed5e5e56d865562522358534a6c",
+    "url": "/booklist/index.html"
+  },
+  {
+    "revision": "f47171567a1986c0b3da",
+    "url": "/booklist/static/css/main.c898ceaf.chunk.css"
+  },
+  {
+    "revision": "fdb2c961dd77ae18bde0",
+    "url": "/booklist/static/js/2.56ca742e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/booklist/static/js/2.56ca742e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f47171567a1986c0b3da",
+    "url": "/booklist/static/js/main.b69eefee.chunk.js"
+  },
+  {
+    "revision": "55a6441c51c928717c8b",
+    "url": "/booklist/static/js/runtime-main.402501eb.js"
+  }
+]);
